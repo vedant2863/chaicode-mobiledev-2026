c699e5a31d1f80c484ad9b8172e6f8acdc51953f
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-03 13:31:52 +0530

    react refresher completed

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from "react";
-import GitHubProfile from "./GitHubProfile";
+import React, { useState } from "react";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchGithubData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          "https://api.github.com/users/aestheticsuraj234",
-        );
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const handleSubmit = (e)=>{
+  e.preventDefault()
+    console.log(e.target.elements.name.value)
+  }
 
-    fetchGithubData();
-  }, []);
 
-  return <GitHubProfile data={data} isLoading={isLoading} />;
+return (<form onSubmit={handleSubmit}>
+    <input name="name"/>
+    <button type="submit">Submit</button>
+  </form>)
 };
 
 export default App;
+
