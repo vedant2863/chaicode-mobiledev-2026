1f782b44c4bbdb6202bf7c910bf46f843d3aa123
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-17 12:56:52 +0530

    native tabs

--- expo-router-101/src/app/(tabs)/_layout.tsx
+++ expo-router-101/src/app/(tabs)/_layout.tsx
@@ -1,30 +1,28 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
-    return (
-        <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: 'Explore',
-            tabBarIcon: ({ color }) => <Ionicons name="compass" size={24} color={color} />,
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: 'Settings',
-            tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
-          }}
-        />
-      </Tabs>
-    )
+  return (
+    <NativeTabs tintColor={"tomato"} backgroundColor={"#111827"}  >
+      <NativeTabs.Trigger name="index">
+
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+
+      </NativeTabs.Trigger>
+
+
+      <NativeTabs.Trigger name="settings">
+
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name='explore'>
+      <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="compass.drawing" md="compare_arrows" />
+
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
